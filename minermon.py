123400a699ce6b2a0a4097f40db8c9dd4d626cfc
Imanol Barberia <imanol.barberia@gmail.com>
2022-04-07 01:32:26 +0200

    some modifications

--- minermon.py
+++ minermon.py
@@ -9,6 +9,10 @@ CFG_USERID_FILE = ".telegram/user_id"
 
 
 def init_bot():
+    """
+    Initialize bot
+    :return: Tuple with the bot handler and the user ID
+    """
     if not os.path.exists(CFG_USERID_FILE):
         print("[ERROR]: File '%s' does not exist. Make sure you create this file and write your user ID there." %
               CFG_USERID_FILE, file=sys.stderr)
@@ -45,19 +49,78 @@ def init_bot():
     return bot, user_id
 
 
-def main():
-    (bot, usr) = init_bot()
-    bot.send_message(usr, "[LOG]: Bot initialized")
+def cmd_start(bot: telebot.TeleBot, msg: telebot.types.Message):
+    """
+    START command
+    :param bot:
+    :param msg:
+    :return:
+    """
+    bot.reply_to(msg, "START command executed")
 
-    @bot.message_handler(commands=['start', 'help'])
-    def send_welcome(message: telebot.types.Message):
-        bot.reply_to(message, "Testing basic commands... Command used: '%s'" % message.text)
+
+def cmd_help(bot: telebot.TeleBot, msg: telebot.types.Message):
+    """
+    HELP command
+    :param bot:
+    :param msg:
+    :return:
+    """
+    pass
+
+
+def cmd_list(bot: telebot.TeleBot, msg: telebot.types.Message):
+    """
+    LIST command
+    :param bot:
+    :param msg:
+    :return:
+    """
+    pass
+
+
+def handle_commands(bot: telebot.TeleBot, msg: telebot.types.Message):
+    """
+    Handle commands in a generic way. Specific command functions are called from this one.
+    :param bot: Bot handler
+    :param msg: Message
+    """
+    cmd = msg.text.split(" ")[0][1:]
+
+    if cmd == "start":
+        cmd_start(bot, msg)
+    elif cmd == "help":
+        cmd_help(bot, msg)
+    elif cmd == "list":
+        cmd_list(bot, msg)
+    else:
+        bot.reply_to(msg, "Unknown command {}".format(cmd))
+
+
+def main():
+    """
+    Main function
+    :return:
+    """
+    available_commands = [
+        'start',
+        'help',
+        'list'
+    ]
+
+    (bot, usr) = init_bot()
+    bot.send_message(usr, "[LOG]: Bot initialized :iphone:")
+
+    @bot.message_handler(commands=available_commands)
+    def hndl_cmds(message: telebot.types.Message):
+        handle_commands(bot, message)
 
     @bot.message_handler(commands=['terminate'])
     def terminate_bot(message: telebot.types.Message):
         bot.reply_to(message, "[LOG] Terminating bot!")
         bot.stop_bot()
 
+    # ToDo: find a better way to terminate the bot
     bot.infinity_polling()
 
 
